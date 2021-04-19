d0600db9a45d72213cf4afe194ffd4f3b7c53dd6
solda <solda@khu.ac.kr>
2021-04-19 23:43:45 +0900

    updated

--- blog/templates/blog/clock.js
+++ blog/templates/blog/clock.js
@@ -2,6 +2,33 @@ const clockContainer = document.querySelector(".js-clock"); // class
 const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
+    var xmlHttp;
+    function srvTime() {
+        try {
+            //FF, Opera, Safari, Chrome
+            xmlHttp = new XMLHttpRequest();
+        } catch (err1) {
+            //IE
+            try {
+                xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (err2) {
+                try {
+                    xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (eerr3) {
+                    //AJAX not supported, use CPU time.
+                    alert("AJAX not supported");
+                }
+            }
+        }
+        xmlHttp.open("HEAD", window.location.href.toString(), false);
+        xmlHttp.setRequestHeader("Content-Type", "text/html");
+        xmlHttp.send("");
+        return xmlHttp.getResponseHeader("Date");
+    }
+
+    var st = srvTime();
+    var date2 = new Date(st);
+
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
@@ -9,7 +36,7 @@ function getTime() {
 
     clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : `${hours}`}:${minutes < 10 ? `0${minutes}` : minutes}:${
         seconds > 9 ? `${seconds}` : `0${seconds}`
-    }`;
+    } ${date2}`;
 }
 
 function init() {
