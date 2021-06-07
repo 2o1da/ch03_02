67063419b250dda2fac1c3f2fda500ba5d38e8ef
Solda <solda@khu.ac.kr>
2021-06-07 21:04:34 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -18,18 +18,17 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_new(request):
-    if request.method== "POST":
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author= request.user
-            post.published_date= timezone.now()
+            post.author = request.user
+            post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm()
+            return redirect('blog.views.post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
-
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
